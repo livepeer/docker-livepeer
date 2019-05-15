92f2f645f0d725c4121e965c7aa4b58a06ec0ac8
Eli Mallon <eli@livepeer.org>
2019-05-15 16:07:08 -0700

    add nvenc to ffmpeg build

--- ffmpeg/install_ffmpeg.sh
+++ ffmpeg/install_ffmpeg.sh
@@ -1,7 +1,16 @@
+#!/usr/bin/env bash
+
 set -ex
 
 export PATH="$HOME/compiled/bin":$PATH
 export PKG_CONFIG_PATH=$HOME/compiled/lib/pkgconfig
+
+if [ ! -e "$HOME/nv-codec-headers" ]; then
+  git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git "$HOME/nv-codec-headers"
+  cd $HOME/nv-codec-headers
+  make -e PREFIX="$HOME/compiled"
+  make install -e PREFIX="$HOME/compiled"
+fi
 
 if [ ! -e "$HOME/nasm/nasm" ]; then
   # sudo apt-get -y install asciidoc xmlto # this fails :(
@@ -18,7 +27,7 @@ if [ ! -e "$HOME/x264/x264" ]; then
   cd "$HOME/x264"
   # git master as of this writing
   git checkout 545de2ffec6ae9a80738de1b2c8cf820249a2530
-  ./configure --prefix="$HOME/compiled" --enable-pic --enable-static ${HOST_OS:-} --disable-cli
+  ./configure --prefix="$HOME/compiled" --enable-pic --enable-static
   make
   make install-lib-static
 fi
@@ -26,11 +35,10 @@ fi
 if [ ! -e "$HOME/ffmpeg/libavcodec/libavcodec.a" ]; then
   git clone --depth 1 -b n4.1 https://git.ffmpeg.org/ffmpeg.git "$HOME/ffmpeg" || echo "FFmpeg dir already exists"
   cd "$HOME/ffmpeg"
-  ./configure ${TARGET_OS:-} --disable-programs --disable-doc --disable-sdl2 --disable-iconv \
+  ./configure --disable-programs --disable-doc --disable-sdl2 --disable-iconv \
     --disable-muxers --disable-demuxers --disable-parsers --disable-protocols \
     --disable-encoders --disable-decoders --disable-filters --disable-bsfs \
     --disable-postproc --disable-lzma \
-    --enable-nvenc \
     --enable-gnutls --enable-libx264 --enable-gpl \
     --enable-protocol=https,rtmp,file \
     --enable-muxer=mpegts,hls,segment --enable-demuxer=flv,mpegts \
@@ -40,8 +48,10 @@ if [ ! -e "$HOME/ffmpeg/libavcodec/libavcodec.a" ]; then
     --enable-filter=aresample,asetnsamples,fps,scale \
     --enable-encoder=aac,libx264 \
     --enable-decoder=aac,h264 \
+    --enable-cuda \
+    --enable-cuvid \
+    --enable-nvenc \
     --prefix="$HOME/compiled"
   make
   make install
 fi
-

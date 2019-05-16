aa0754fa4d0bfffbb9b86fb683f6c45511634968
Eli Mallon <eli@livepeer.org>
2019-05-16 10:54:13 -0700

    Consolidate install_ffmpeg_windows.sh in install_ffmpeg.sh

--- ffmpeg/install_ffmpeg.sh
+++ ffmpeg/install_ffmpeg.sh
@@ -5,6 +5,20 @@ set -ex
 export PATH="$HOME/compiled/bin":$PATH
 export PKG_CONFIG_PATH=$HOME/compiled/lib/pkgconfig
 
+# Windows (MSYS2) needs a few tweaks
+if [[ $(uname) == *"MSYS2_NT"* ]]; then
+  export PATH="$PATH:/usr/bin:/mingw64/bin"
+  export C_INCLUDE_PATH="${C_INCLUDE_PATH:-}:/msys64/mingw64/lib"
+  export HOME="/build"
+  mkdir -p $HOME
+
+  export PATH="$HOME/compiled/bin":$PATH
+  export PKG_CONFIG_PATH=$HOME/compiled/lib/pkgconfig:/mingw64/lib/pkgconfig
+
+  export TARGET_OS="--target-os=mingw64"
+  export HOST_OS="--host=x86_64-w64-mingw32"
+fi
+
 # NVENC only works on Windows/Linux
 if [ $(uname) != "Darwin" ]; then
   if [ ! -e "$HOME/nv-codec-headers" ]; then

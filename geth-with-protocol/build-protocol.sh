ee511758cf552c6022cc61a875a8fb6de81e0f9b
RiccardoBiosas <rbios@protonmail.com>
2022-04-29 12:15:07 +0200

    updated the geth-with-protocol docker to use the confluence branch and hardhat

--- geth-with-protocol/build-protocol.sh
+++ geth-with-protocol/build-protocol.sh
@@ -1,89 +1,31 @@
 mkdir /psrc && cd /psrc
 
-git clone -b streamflow https://github.com/livepeer/protocol.git
+# to be changed after rb/hardhat-geth-config is merged with the confluence branch
+git clone -b rb/hardhat-geth-config https://github.com/livepeer/protocol.git
 srcDir=/psrc
 cd $srcDir/protocol
-# TODO: Update this file to be compatible with new deployment scripts in protocol repo
-git checkout d8e3523e3779a7f339b94159ad5d36ce428799cc
-echo "Setting devenv specific protocol parameters"
-migrations="$srcDir/protocol/migrations/migrations.config.js"
-sed -i 's/roundLength:.*$/roundLength: 50,/' $migrations
-sed -i 's/unlockPeriod:.*$/unlockPeriod: 50,/' $migrations
-sed -i 's/numActiveTranscoders:.*$/numActiveTranscoders: 50,/' $migrations
-sed -i 's/numTranscoders:.*$/numTranscoders: 100,/' $migrations
-
-cat << EOF > $srcDir/protocol/truffle.js
-require("babel-register")
-require("babel-polyfill")
-
-module.exports = {
-    networks: {
-        development: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*", // Match any network id
-            gas: 8000000
-        },
-        lpTestNet: {
-            from: "0x$GETH_MINING_ACCOUNT",
-            host: "localhost",
-            port: 8545,
-            network_id: 54321,
-            gas: 8000000
-        }
-    },
-    compilers: {
-        solc: {
-            version: "0.5.11",
-            parser: "solcjs",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200
-                }
-            }
-        }
-    }
-};
-EOF
-
-echo "Installing local dev version of $srcDir/protocol/scripts/unpauseController.js"
-
-cat <<EOF > $srcDir/protocol/scripts/unpauseController.js
-const Controller = artifacts.require("Controller")
-
-module.exports = async cb => {
-    const controller = await Controller.deployed()
-    await controller.unpause({from: "0x$GETH_MINING_ACCOUNT"})
-    cb()
-}
-EOF
-
-echo "npm install"
-npm install
-echo "Compiling contracts..."
-npm run compile
-echo "Done compiling, deploying..."
-
-# Create truffle alias pointing to locally installed version
-alias truffle=./node_modules/.bin/truffle
 
 nohup bash -c "/start.sh &" &&
 sleep 1
 
 OPWD=$PWD
 cd $srcDir/protocol
-migrateCmd="npm run migrate -- --network=lpTestNet"
+echo "yarn install"
+yarn install
+echo "npx hardhat typechain"
+npx hardhat typechain
+migrateCmd="yarn deploy --network geth"
 echo "Running $migrateCmd"
 eval $migrateCmd
-unpauseCmd="truffle exec --network lpTestNet scripts/unpauseController.js"
+unpauseCmd="npx hardhat unpause --network geth"
 echo "Running $unpauseCmd"
 eval $unpauseCmd
-controllerAddress=$(cd $srcDir/protocol && truffle networks | awk '/54321/{f=1;next} /TokenPools/{f=0} f' | grep Controller | cut -d':' -f2 | tr -cd '[:alnum:]')
+controllerAddress=$(npx hardhat print-contract-address --contract Controller --network geth)
 echo "Controller address: $controllerAddress"
-truffle networks
-truffle networks > $gethRoot/networks
 echo $controllerAddress > $gethRoot/controllerAddress
+migrateArbitrumLPTMockCmd="npx hardhat deploy --tags ARBITRUM_LPT_MOCK --network geth"
+echo "Running $migrateArbitrumLPTMockCmd"
+eval $migrateArbitrumLPTMockCmd
 cd $OPWD
 sleep 3
 # now we need to wait till 100 block mined (so current protocol round will be 2)
